a09a10b0ea5dbfdd105141a8370b915784a2cdd1
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-05 16:55:00 +0600

    add model department

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205102341) do
+ActiveRecord::Schema.define(version: 20161205103230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20161205102341) do
     t.text     "comment"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
