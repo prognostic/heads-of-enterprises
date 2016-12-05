36a82e65c5f571a839dbd67428e15f54ea03c79c
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-05 16:26:20 +0600

    add model bank

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205101002) do
+ActiveRecord::Schema.define(version: 20161205102341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "banks", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
