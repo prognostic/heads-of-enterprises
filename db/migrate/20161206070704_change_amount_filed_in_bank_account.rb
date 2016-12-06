cc5f6ac223a543cc7253412fc393460c2f3844b7
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-06 13:18:44 +0600

    change amount field in the bank account model

---
+++ db/migrate/20161206070704_change_amount_filed_in_bank_account.rb
@@ -0,0 +1,5 @@
+class ChangeAmountFieldInBankAccount < ActiveRecord::Migration[5.0]
+  def change
+    change_column :bank_accounts, :amount_of_money, :decimal, precision: 10, scale: 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206062905) do
+ActiveRecord::Schema.define(version: 20161206070704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20161206062905) do
   create_table "bank_accounts", force: :cascade do |t|
     t.date     "opening_date"
     t.integer  "status"
-    t.float    "amount_of_money"
+    t.decimal  "amount_of_money", precision: 10, scale: 2
     t.text     "comment"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.integer  "branch_id"
     t.integer  "company_id"
     t.index ["branch_id"], name: "index_bank_accounts_on_branch_id", using: :btree
