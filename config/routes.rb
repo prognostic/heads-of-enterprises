75ddb2dc96ee70fda89e8eee55df0d08a0bfc733
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-07 10:25:00 +0600

    generate the cities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  #root 'pages#main'
+
+  resources :cities
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
