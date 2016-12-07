74ca5e63bf2bf2d450f184c5e52000fce88abed5
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-07 12:43:18 +0600

    add the template

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,34 @@ module CompaniesDirectory
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # setup bower components folder for lookup
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    # fonts
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    # images
+    config.assets.precompile << /\.(?:png|jpg)$/
+    # precompile vendor assets
+    config.assets.precompile += %w( base.js )
+    config.assets.precompile += %w( base.css )
+    # precompile themes
+    config.assets.precompile += ['angle/themes/theme-a.css',
+                                 'angle/themes/theme-b.css',
+                                 'angle/themes/theme-c.css',
+                                 'angle/themes/theme-d.css',
+                                 'angle/themes/theme-e.css',
+                                 'angle/themes/theme-f.css',
+                                 'angle/themes/theme-g.css',
+                                 'angle/themes/theme-h.css'
+    ]
+    # Controller assets
+    config.assets.precompile += [
+        # Scripts
+        'singleview.js',
+        # Stylesheets
+        'singleview.css'
+    ]
+
+
   end
 end
