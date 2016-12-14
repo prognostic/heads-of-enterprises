0ccea088cf091e4ac8e20fb09c72e6cf1e5e6cf8
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-14 17:49:50 +0600

    add a blank control panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: "dashboard#index"
 
   scope 'control' do
+    root to: "dashboard#index"
     resources :cities, :banks, :branches, :companies, :faces, :bank_accounts, :assignments
+    get 'dashboard', to: 'dashboard#index'
   end
 
   devise_for :users, path_names: {
