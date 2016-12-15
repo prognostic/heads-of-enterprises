7db4e7a26d016c4756b882ce708ed8fa4d08d9e7
Sergey Krasnodemskiy <sergeykrasnodemskiy@gmail.com>
2016-12-15 20:11:48 +0600

    add a translate the datepicker

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -10,6 +10,34 @@ $("form").on("cocoon:after-insert", function(_, row){
     init_select2(field);
 });
 
-$(function() {
-    $('.datepicker').datepicker();
+$(function () {
+    $('.datepicker').datepicker($.extend({
+            inline: true,
+            changeYear: true,
+            changeMonth: true,
+        }
+    ));
+});
+
+jQuery(function ($) {
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: '&#x3c;Пред',
+        nextText: 'След&#x3e;',
+        currentText: 'Сегодня',
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+            'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        weekHeader: 'Нед',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
 });
